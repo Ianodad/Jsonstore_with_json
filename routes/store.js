e37c2dffb32e56639bb8a798bfbcc007e162b558
Ian Odhiambo <you@example.com>
2019-10-01 19:08:58 +0300

    in store created for view

--- routes/store.js
+++ routes/store.js
@@ -6,17 +6,33 @@ const router = express.Router();
 const users = require('../services/Users');
 const products = require('../services/Products')
 const comments = require('../services/Comments')
+const category = require('../services/Category')
+const company = require('../services/Company')
+
 
 
 
 // Store page 
-router.get('/', (req, res) => res.render('store', {
-    title: 'Store View',
-    style: 'store.css',
-    users,
-    products,
-    comments
+router.get('/', (req, res) => {
 
-}))
+    const Users = users.slice(0, 3)
+    const Products = products.slice(0, 3)
+    const Comments = comments.slice(0, 3)
+    const Category = category.slice(0, 3)
+    const Company = company.slice(0, 3)
+
+
+
+    res.render('store', {
+        title: 'Store View',
+        style: 'store.css',
+        Users,
+        Products,
+        Comments,
+        Category,
+        Company
+
+    })
+})
 
 module.exports = router;
